369677bd94a966b5cf69944cff549ae4cd250e5f
Wyzbits <wyzbits@gmail.com>
2023-03-20 16:48:30 +0000

    made header sticky

--- core/components/warpper/header/Header.tsx
+++ core/components/warpper/header/Header.tsx
@@ -5,7 +5,7 @@ import MenuButton from "./MenuButton";
 
 const Header = () => {
   return (
-    <header className="md:h-32 w-screen border-b-0.5 border-blurrRed  flex items-center justify-between">
+    <header className="md:h-32 w-screen sticky top-0 flex items-center justify-between z-10 bg-black">
       <MenuButton />
       <Logo />
       <Ippo />
