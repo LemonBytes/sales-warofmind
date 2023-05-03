430d4440583974bcd316777cada436d495c90a35
Wyzbits <wyzbits@gmail.com>
2023-05-03 15:00:26 +0200

    changed logo size

--- core/components/grid/Article.tsx
+++ core/components/grid/Article.tsx
@@ -14,7 +14,7 @@ const Article: React.FC<IArticle> = ({
 }) => {
   return (
     <article
-      className={`${rowSpan} ${colEnd} overflow-hidden w-full text-black font-capriola flex flex-col  items-center outline overflow-hidden relative  `}
+      className={`${rowSpan} ${colEnd} overflow-hidden w-full text-black font-capriola flex flex-col  items-center outline overflow-hidden relative p-10`}
     >
       <Link href={articleSrc} target="_blank">
         <div className="w-full h-full border-box overflow-hidden">
@@ -27,12 +27,12 @@ const Article: React.FC<IArticle> = ({
           />
         </div>
       </Link>
-      <div className="flex justify-evenly items-center min-h-[100px] max-h-[150px] p-5">
-        <h5 className="py-2 px-1 text-black text xl:text-base lg:text-md md:text-xs w-[50%] truncate">
+      <div className="flex justify-around items-center min-h-[20%]  p-5">
+        <h5 className="py-2 px-1 text-black text xl:text-base lg:text-md md:text-xs w-[50%] text-ellipsis">
           {name}
         </h5>
         <div className="flex-col items-center">
-          <p className="xl:text-xl lg:text-base md:text-sm p-3 md:text-xs underline text-black">
+          <p className="xl:text-xl lg:text-base md:text-sm text-base p-3 underline text-black">
             {specialPrice}
           </p>
           <p className="xl:text-base p-1 lg:text-sm md:text-xs line-through">
